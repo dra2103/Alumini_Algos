0d55c7ea38f22cb3a178b4951746529ede2707d4
Daniel Adamski <dra2103@gmail.com>
2022-03-18 11:20:58 -0700

    sorted

---
+++ findMinInRotatedSortedArray.js
@@ -0,0 +1,26 @@
+var findMin = function (nums) {
+    if (nums.length === 0) {
+        return -1
+    }
+    let start = 0
+    let end = nums.length - 1
+    let mid
+    while (start <= end) {
+        mid = start + Math.floor((end - start) / 2)
+
+        if (nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+            return nums[mid]
+
+        }
+        if (nums[mid] < nums[end]) {
+            end = mid - 1
+        }
+        else if (nums[mid] > nums[end]) {
+            start = mid + 1
+        }
+        else if (mid == end && mid == start)
+            return nums[mid]
+
+
+    } return nums[mid]
+};
